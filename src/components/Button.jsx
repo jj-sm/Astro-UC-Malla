165ef5029887a8b01f38bbc3742d72dd392ec93b
juanjo <jjsanchezmedina@gmail.com>
2025-04-03 14:20:47 -0300

    Mobile compatibility improved

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ import { CourseContext } from "../context/CourseContext";
 export const Button = ({ className }) => {
     const { handleReset, setViewSigla } = useContext(CourseContext);
     return (
-        <div className="flex space space-x-2">
+        <div className="flex space-x-2">
             <button
                 type="button"
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-1.5 mb-1 mt-1 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
