bb89bb85989c428af394338c9a7cf96dc6d630d4
juanjo <jjsanchezmedina@gmail.com>
2025-04-25 17:47:51 -0400

    Footer link changed

--- src/utils/test.js
+++ src/utils/test.js
@@ -1,7 +1,6 @@
 import { findCourseNameById } from './findCourseNameById.js';
 import { courses } from './malla-AST-020101-1.js';
 
-// Test the function
 const testId = 12;
 const courseName = findCourseNameById(testId, courses);
-console.log(`Course name for ID ${testId}: ${courseName}`); // Should print the course name for ID 12
+console.log(`Course name for ID ${testId}: ${courseName}`);
